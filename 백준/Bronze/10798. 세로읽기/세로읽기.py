08c2ef3cdcd0761f61e0d2329f3b6c854cec02ae
Juyeong Jin <pearlzero21@gmail.com>
2025-04-23 15:18:26 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    words = [input() for i in range(5)]
+
+    for j in range(15):
+        for i in range(5):
+            if j < len(words[i]):
+                print(words[i][j], end='')
