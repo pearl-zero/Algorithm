000580adfca7bd5b02339a53015a6cd2eca308c1
Juyeong Jin <pearlzero21@gmail.com>
2025-04-23 15:22:41 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,14 @@
+n = int(input())
+array = [[0]*100 for _ in range(100)]
+for _ in range(n):
+    y1, x1 = map(int, input().split())
+        
+    for i in range(x1, x1 + 10):
+        for j in range(y1, y1 + 10):
+            array[i][j] = 1
+
+result = 0
+for k in range(100):
+    result += array[k].count(1)
+        
+print(result)
