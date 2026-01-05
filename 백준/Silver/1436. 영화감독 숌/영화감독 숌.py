110e375122ca85419e0fabf9f43fe82fc61b61a0
Juyeong Jin <pearlzero21@gmail.com>
2026-01-05 17:40:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 668 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+
+count = 0
+num = 666
+
+while True : 
+    if '666' in str(num):
+        count += 1
+        
+    if count == n:
+        print(num)
+        break
+   
+    num += 1
